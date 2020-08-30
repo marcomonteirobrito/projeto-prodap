5545c4738245fd816a2b7db2c9a274c29280e9a5
Marco <marco_monteiro@live.com>
2020-08-30 00:51:30 -0300

    Inclusão de testes

---
+++ backend/__tests__/integration/salary.test.js
@@ -0,0 +1,14 @@
+const { Salary } = require('../../src/app/models/Salary');
+
+describe('Calculo de salário;', () => {
+  it('A rota /salary com método post deve retornar um JSON quando um novo calculo de salário for criado', 
+    async () => {
+      const salary = await Salary.create({ 
+        name: 'Marco',
+        fixedSalary: '500.00',
+        totalSale: '1230.30'
+      });
+
+      expect(salary.total_salary).toBe(684.545);
+  });
+});
